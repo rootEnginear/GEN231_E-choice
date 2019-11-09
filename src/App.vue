376f219e06dc9b2bcedae3327145beaf27b69906
rootEnginear <moo_suthep@hotmail.com>
2019-11-09 22:57:20 +0700

    + Set things up

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,36 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
+    <b-navbar class="is-fixed-top" :shadow="true">
+      <template slot="brand">
+        <b-navbar-item tag="router-link" :to="{ path: '/' }">
+          <img
+            src="https://raw.githubusercontent.com/buefy/buefy/dev/static/img/buefy-logo.png"
+            alt="Lightweight UI components for Vue.js based on Bulma"
+          />
+        </b-navbar-item>
+      </template>
+      <template slot="start">
+        <b-navbar-item tag="router-link" to="/">Home</b-navbar-item>
+        <b-navbar-item tag="router-link" to="/about">About</b-navbar-item>
+      </template>
+    </b-navbar>
+    <!-- <div id="nav">
+      <router-link to="/">Home</router-link>|
       <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    </div>-->
+    <transition name="routerview">
+      <router-view />
+    </transition>
   </div>
 </template>
 
+<script>
+export default {};
+</script>
+
 <style lang="scss">
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
@@ -25,5 +45,40 @@
       color: #42b983;
     }
   }
+}
+
+.routerview-enter-active {
+  transition: opacity 0.3s cubic-bezier(0, 0, 0.2, 1);
+  transition: transform 0.3s cubic-bezier(0, 0, 0.2, 1);
+}
+.routerview-leave-active {
+  transition: opacity 0.3s cubic-bezier(0.4, 0, 1, 1);
+  transition: transform 0.3s cubic-bezier(0.4, 0, 1, 1);
+}
+.routerview-leave-to {
+  transform: translateX(-100vw);
+}
+.routerview-enter {
+  transform: translateY(100vh);
+}
+.routerview-leave-to,
+.routerview-enter {
+  opacity: 0;
+}
+.routerview-enter-active,
+.routerview-leave-active {
+  transition-property: transform, opacity;
+  transition-duration: 0.5s;
+}
+.routerview-enter-active,
+.routerview-leave-active,
+.routerview-enter,
+.routerview-leave-to {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  transform-origin: top left;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,61 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import './registerServiceWorker'
+// Vue
+import Vue from "vue";
+Vue.config.productionTip = false;
 
-Vue.config.productionTip = false
+// Buefy
+import Buefy from "buefy";
+import "buefy/dist/buefy.css";
+Vue.use(Buefy);
 
+// Router
+import Router from "vue-router";
+Vue.use(Router);
+
+function loadView(view) {
+  return () => import(`./views/${view}.vue`);
+}
+
+const router = new Router({
+  routes: [
+    {
+      path: "/",
+      name: "home",
+      component: loadView("Home"),
+      meta: {
+        title: "หน้าหลัก"
+      }
+    },
+    {
+      path: "/about",
+      name: "about",
+      component: loadView("About"),
+      meta: {
+        title: "เกี่ยวกับ"
+      }
+    },
+    {
+      path: "*",
+      redirect: "/"
+    }
+  ]
+});
+
+// eslint-disable-next-line no-unused-vars
+router.beforeEach((to, from, next) => {
+  const customTitle = to.matched
+    .slice()
+    .reverse()
+    .find(record => record.meta && record.meta.title);
+  document.title = `✅ E-choice : ${customTitle ? customTitle.meta.title : ""}`;
+  next();
+});
+
+// PWA
+import "./registerServiceWorker";
+
+// App
+import App from "./App.vue";
 new Vue({
   router,
   render: h => h(App)
-}).$mount('#app')
+}).$mount("#app");
